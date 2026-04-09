c21eb3241cec47bdc67a613d01d09fb6142cf8cf
bturkan <bayramovat2005@gmail.com>
2026-04-09 13:56:22 +0400

    php start

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- sol teref acarlar    sag teref deyerler -->
+     <!-- print sade stringler ucundur   print r ise arraylari ve obyektleri cap etmek ucundur -->
+      <!-- oz yazdigimiz acarlar varsa obyektdir   proqram ozu teyin edirse arraydir -->
+<?php
+
+// echo "hemide turkan";
+
+  echo "<pre>"; 
+      print_r(
+          [
+               'ad'=> 'Ali',
+               'soyad'=> 'Veli',
+                'yas'=> 30
+          ]
+      );
+     echo "<pre>";
+
+?>
+
+
+<!-- <script>
+    let a = {
+        'ad': 'Ali'
+      'soyad':'Veli'    JAVA SCRIPT
+      'yas': 30
+    }
+</script> -->
+
+
+
+
+</body>
+</html>
