3378630e801db3e5939cab3e9b3e0a00a4af64da
bturkan <bayramovat2005@gmail.com>
2026-05-08 09:36:23 +0400

    introduce

--- avtomobil.php
+++ avtomobil.php
@@ -1,45 +1,103 @@
 <?php
 
+    class Person {
 
-   class Car {
-
-    // propery
-      public $color;
-      public $model;
+      public $name;
+      public $age;
 
 
 
-    //  metod
-      public function start() {
-        echo "Avtomobil ise dusdu. <hr>";
+      public function introduce () {
+         echo  "My name is " . $this->name .  " and I am " .  $this->age .   " years old. ";
+
       }
-   }
+    }
 
-   $bmw = new Car ();
-   $bmw->color="qirmizi";
-   $bmw->model="Bmw X5";
+    $person1 = new Person();
+    $person1->name =  "Jhon" ;
+    $person1->age  =  30;
 
-   echo $bmw->color . "<br>";
-   echo $bmw->model . "<br>";
-    $bmw->start();
+    $person1->introduce();
 
-   $mercedes = new Car ();
-   $mercedes->color="boz";
-   $mercedes->model="mercedes S-class";
 
-   echo $mercedes->color . "<br>";
-   echo $mercedes->model . "<br>";
-    $mercedes->start();
+
+    
 
 
 
 
-    $dodge = new Car ();
-    $dodge->color="qara";
-    $dodge->model="dodge hellcat";
 
-    echo $dodge->color . "<br>";
-    echo $dodge->model . "<br>";
-    $dodge->start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //  class Car {
+
+  //   // propery
+  //     public $color;
+  //     public $model;
+
+
+
+  //   //  metod
+  //     public function start() {
+  //       echo "Avtomobil ise dusdu. <hr>";
+  //     }
+  //  }
+
+  //  $bmw = new Car ();
+  //  $bmw->color="qirmizi";
+  //  $bmw->model="Bmw X5";
+
+  //  echo $bmw->color . "<br>";
+  //  echo $bmw->model . "<br>";
+  //   $bmw->start();
+
+  //  $mercedes = new Car ();
+  //  $mercedes->color="boz";
+  //  $mercedes->model="mercedes S-class";
+
+  //  echo $mercedes->color . "<br>";
+  //  echo $mercedes->model . "<br>";
+  //   $mercedes->start();
+
+
+
+
+  //   $dodge = new Car ();
+  //   $dodge->color="qara";
+  //   $dodge->model="dodge hellcat";
+
+  //   echo $dodge->color . "<br>";
+  //   echo $dodge->model . "<br>";
+  //   $dodge->start();
 
 
