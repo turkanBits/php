797bd02b0bf3c03ad0d2d3226c2f555f64d3ac02
bturkan <bayramovat2005@gmail.com>
2026-06-22 06:41:41 +0400

    DOM

--- index.php
+++ index.php
@@ -1,70 +1,137 @@
 <?php
-class ApiResponse
-{
-    private $statusCode;
-    private $headers=[];
-    private $data;
 
-    public function __construct(int $statusCode=200)
-    {
-        $this->statusCode=$statusCode;
-    }
-
-    public function addHeader(string $key,string $value) {
-      $this->headers[]="$key:$value";
-
-      // echo "<pre>";
-      // print_r($this->headers);
-      // echo "</pre>";
-
-    }
-
-    public function setData(array $data) {
-      $this->data=$data;
-    }
-
-    public function send() {
-
-        http_response_code($this->statusCode);
-
-        foreach ($this->headers as $header) {
-          echo "<pre>";
-            print_r($header);
-          echo "</pre>";  
-        }
-
-    }
-}
+// DOM ve NODE
 
  
-$response =new ApiResponse();
+class MenuManager 
+{
 
-$response->addHeader('Content-Type','application/json');
-$response->addHeader('Cache-Control','no-cache');
+   private $dom;
+   private $menu;
+
+   public function __construct($filePath) {
+
+
+      $this->dom=new DOMDocument();
+      $this->dom->load($filePath);
+      $this->menu=$this->dom->getElementsBYTagName("menu");
+
+      print_r($this->menu->item(0));
+
+   }
+
+   public function addDish($name,$price) {
+
+   }
+
+}
+
+$menuManager=new MenuManager("menu.xml");
+$menuManager->addDish("Burger","8.99");
+
+
+// echo "Yeni yemek menyusu elave edilir";
 
 
 
-$data=[
-  'succes'=>true,
-  'message'=>'Melumat ugurla elde edildi',
-  'data'=> [
-    [
-          "id"=> 1,
-          "name"=> "Telefon",
-          "price"=> 1000,
-          "stock"=> 50
-    ],
-    [
-          "id"=> 2,
-          "name"=> "Laptop",
-          "price"=> 2000,
-          "stock"=> 20
-    ]
 
-]
 
-    ];
 
-    $response->setData($data);
 
-    $response->send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class ApiResponse
+// {
+//     private $statusCode;
+//     private $headers=[];
+//     private $data;
+
+//     public function __construct(int $statusCode=200)
+//     {
+//         $this->statusCode=$statusCode;
+//     }
+
+//     public function addHeader(string $key,string $value) {
+//       $this->headers[]="$key:$value";
+
+//       // echo "<pre>";
+//       // print_r($this->headers);
+//       // echo "</pre>";
+
+//     }
+
+//     public function setData(array $data) {
+//       $this->data=$data;
+//     }
+
+//     public function send() {
+
+//         http_response_code($this->statusCode);
+
+//         foreach ($this->headers as $header) {
+//           echo "<pre>";
+//             print_r($header);
+//           echo "</pre>";  
+//         }
+
+//     }
+// }
+
+ 
+// $response =new ApiResponse();
+
+// $response->addHeader('Content-Type','application/json');
+// $response->addHeader('Cache-Control','no-cache');
+
+
+
+// $data=[
+//   'succes'=>true,
+//   'message'=>'Melumat ugurla elde edildi',
+//   'data'=> [
+//     [
+//           "id"=> 1,
+//           "name"=> "Telefon",
+//           "price"=> 1000,
+//           "stock"=> 50
+//     ],
+//     [
+//           "id"=> 2,
+//           "name"=> "Laptop",
+//           "price"=> 2000,
+//           "stock"=> 20
+//     ]
+
+// ]
+
+//     ];
+
+//     $response->setData($data);
+
+//     $response->send();
