4144b4d4d90e6f45f29e653e37501f0afd29ef8a
bturkan <bayramovat2005@gmail.com>
2026-07-08 13:57:59 +0400

    currency

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     </select>
 
     <button id ="show_exchange_btn">Hesabla</button>
-    
+
 
     <p id="show_exchange">Netice burada gosterilecek...</p>
     
@@ -26,7 +26,11 @@
     <script>
         document.getElementById("show_exchange_btn").addEventListener("click",function () {
 
-                console.log('duymeni basdiniz');
+            const amount    =    document.getElementById("number").value;
+            const currency  =    document.getElementById("currency").value;
+
+            console.log(amount);
+            console.log(currency);
         })
     </script>
 </body>
