5fa8e153d7a2194aa7307a515a9e322a0e07c9d7
bturkan <bayramovat2005@gmail.com>
2026-07-10 11:13:39 +0400

    defined

--- index.php
+++ index.php
@@ -5,16 +5,37 @@
 
 // password_hash($password,PASSWORD_DEFAULT)
 
-$hash = password_hash("123456",PASSWORD_DEFAULT);
+// $hash = password_hash("123456",PASSWORD_DEFAULT);
 
-// echo $hash;
+// // echo $hash;
 
 
-if ( password_verify("123456" , $hash)) {
+// if ( password_verify("123456" , $hash)) {
 
-    echo "Parol dogrudur"; 
+//     echo "Parol dogrudur"; 
 
-}    
+// }    
 
     
 
+// date()
+
+// strtotime()
+
+// time()
+
+// echo date ("Y-m-d H:i:s");
+
+
+// defined()
+
+define ("PI",3.14);
+
+if (defined ("PI") ) {
+    echo "PI deyeri: " . PI;
+
+} else {
+
+    echo "PI deyeri teyin olunmayib";
+
+}
