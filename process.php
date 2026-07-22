1707d5e2ded1d9cb4a815c732b4c227d7b0c3cc2
bturkan <bayramovat2005@gmail.com>
2026-07-22 15:58:40 +0400

    FFF

--- process.php
+++ process.php
@@ -2,10 +2,29 @@
 
 if($_SERVER['REQUEST_METHOD' ] === 'POST' ) {
     
-   echo "<pre>";
-      print_r($_FILES['file'] );
-   echo "</pre>" ;  
+ if(isset($_FILES['file'] ) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
 
- 
+   $fileImpPath=$_FILES['file']['tmp_name'];
+   $fileName=$_FILES['file']['name'];
+   $fileSize=$_FILES['file']['size'];
+   $fileType=$_FILES['file']['type'];
+
+   $uploadPath = 'uploads/' . $fileName;
+
+   if($fileSize > 3 * 1024 *1024) {
+      echo "Fayl boyukdur.Maksimum 3MB ola biler.";
+      exit();
+   }
+
+   if(move_uploaded_file($fileImpPath,$uploadPath) ) {
+      echo "Fayl yuklendi: " .$fileName;
+
+   }else {
+      echo "Fayl yuklendikde xeta bas verdi.";
+   }
+
+} else {
+   echo "Fayl yuklenmedi";
+}
 
 }
